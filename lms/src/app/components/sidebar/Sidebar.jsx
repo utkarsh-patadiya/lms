2f19d433591a603d9517c25e528a36c2f7a5ca49
utkarsh <patadiyautkarsh@gmail.com>
2024-05-02 22:11:33 +0530

    added sidebar

--- lms/src/app/components/sidebar/Sidebar.jsx
+++ lms/src/app/components/sidebar/Sidebar.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Count from "../count/Count";
+
 function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="flex h-screen">
-    <div className="bg-gray-700 text-gray-300 rounded-lg p-4 pr-6">Sidebar</div>
-    <div className="flex justify-center items-center flex-1">
-      <Count />
-    </div>
-  </div>
+    <>
+      {!isOpen && (
+        <button
+          className="bg-gray-700 text-gray-300 rounded-lg p-2 text-sm"
+          onClick={toggleSidebar}
+        >
+          Open Sidebar
+        </button>
+      )}
+      <div className="flex" style={{ height: 'calc(100vh - 100px)' }}>
+        {isOpen && (
+          <div className="bg-gray-700 text-gray-300 rounded-lg p-4 pr-6">
+            Sidebar
+            <button className="ml-2" onClick={toggleSidebar}>
+              Close
+            </button>
+          </div>
+        )}
+        <div className="flex justify-center items-center flex-1">
+          <Count />
+        </div>
+      </div>
+    </>
   );
 }
 
